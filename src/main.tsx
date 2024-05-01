ca7f2ee659e9b75aef2f44aa2c9de28781cc19c6
nerometa <nerometa@protonmail.com>
2024-05-02 00:05:01 +0700

    chore: organize imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.tsx';
 import './index.css';
-import { ChakraProvider } from '@chakra-ui/react';
-import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store/store.ts';
-import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
